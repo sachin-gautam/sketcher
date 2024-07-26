20e9005f6d1698d1000887fb0c0bbdc754018ca4
sachin-gautam <gautamsachin757@gmail.com>
2024-07-26 20:53:20 +0545

    ok

--- scripts.js
+++ scripts.js
@@ -9,45 +9,39 @@ btn.textContent = "Change Dimension";
 
 function createGrids(size){
     container.innerHTML = "";
+    const itemSize = 100/size;
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
           // Create a grid item element
           const gridItem = document.createElement("div");
           gridItem.className = "grid-item";
-          gridItem.style.flex = `0 0 ${100/size}%`;
+          gridItem.style.flex = `0 0 ${itemSize}%`;
+          gridItem.style.height = `${itemSize}%`
           container.appendChild(gridItem);
         }
     }
-}
-
-const gridItems = document.querySelectorAll(".grid-item");
-
-gridItems.forEach(checkHover);
-function checkHover(gridItem){
-    gridItem.addEventListener("mouseover",() => {
-    gridItem.style.cssText = "background-color: red;";
-    console.log(gridItem);});
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach(gridItem => {
+        gridItem.addEventListener("mouseover",() => {
+            gridItem.style.cssText = "background-color: red;";
+            });
+    });
 }
 
 
 btn.addEventListener("click", ()=>{
-    size = prompt("Enter the size of the grid");
-    if(size<100){
+    let newSize = prompt("Enter the size of the grid");
+    newSize = parseInt(newSize);
+    if(newSize<=100 && newSize>0){
+        size = newSize;
         createGrids(size);
     }
     else{
         alert("Exceeds the size limit!");
-        location.reload();
+        // location.reload();
     }
 
 });
-
-gridItems.forEach(checkHover);
-function checkHover(gridItem){
-    gridItem.addEventListener("mouseover",() => {
-    gridItem.style.cssText = "background-color: red;";
-    console.log(gridItem);});
-}
 
 
 
