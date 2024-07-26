815117dba8994ee8fd0432fc278f3ba5c291dcab
sachin-gautam <gautamsachin757@gmail.com>
2024-07-26 19:41:06 +0545

    added custom dimension

--- scripts.js
+++ scripts.js
@@ -1,26 +1,53 @@
 const container = document.querySelector("#container");
 
+let size = 16;
 
+createGrids(size);
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-      // Create a grid item element
-      const gridItem = document.createElement("div");
-      gridItem.className = "grid-item";
-      container.appendChild(gridItem);
+const btn = document.querySelector("#btn");
+btn.textContent = "Change Dimension";
+
+function createGrids(size){
+    container.innerHTML = "";
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+          // Create a grid item element
+          const gridItem = document.createElement("div");
+          gridItem.className = "grid-item";
+          gridItem.style.flex = `0 0 ${100/size}%`;
+          container.appendChild(gridItem);
+        }
     }
 }
 
 const gridItems = document.querySelectorAll(".grid-item");
 
-gridItems.forEach((gridItem) => {
+gridItems.forEach(checkHover);
+function checkHover(gridItem){
     gridItem.addEventListener("mouseover",() => {
-        gridItem.style.cssText = "background-color: red;";
-        console.log(gridItem);
-    });
-})
+    gridItem.style.cssText = "background-color: red;";
+    console.log(gridItem);});
+}
 
 
+btn.addEventListener("click", ()=>{
+    size = prompt("Enter the size of the grid");
+    if(size<100){
+        createGrids(size);
+    }
+    else{
+        alert("Exceeds the size limit!");
+        location.reload();
+    }
+
+});
+
+gridItems.forEach(checkHover);
+function checkHover(gridItem){
+    gridItem.addEventListener("mouseover",() => {
+    gridItem.style.cssText = "background-color: red;";
+    console.log(gridItem);});
+}
 
 
 
